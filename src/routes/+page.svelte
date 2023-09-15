93951ce14e36f155b1ca66c2fb7e010d3ab46191
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-15 12:59:15 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,44 @@
-<!-- +page.svelte -->
-
 <script>
-  // You can add any necessary logic here
+  // You can define any logic or data here if needed
 </script>
 
 <style>
+  /* Style the container for the button row */
   .button-row {
     display: flex;
     justify-content: space-between;
+    align-items: center;
   }
 
+  /* Style the individual buttons */
   .button {
     width: 230px;
     height: 103px;
     border-radius: 8px;
-    background-color: #007bff; /* You can change the background color */
-    color: #fff; /* You can change the text color */
+    background-color: #0074d9; /* You can change the background color */
+    color: white; /* Text color */
+    font-size: 18px; /* Text size */
     display: flex;
-    align-items: center;
     justify-content: center;
+    align-items: center;
     cursor: pointer;
+    margin: 5px; /* Adjust the margin between buttons as needed */
   }
 </style>
 
 <div class="button-row">
-  <div class="button" on:click={() => /* Handle button 1 click */}>
-    Button 1
-  </div>
-  <div class="button" on:click={() => /* Handle button 2 click */}>
-    Button 2
-  </div>
-  <div class="button" on:click={() => /* Handle button 3 click */}>
-    Button 3
-  </div>
-  <div class="button" on:click={() => /* Handle button 4 click */}>
-    Button 4
-  </div>
-  <div class="button" on:click={() => /* Handle button 5 click */}>
-    Button 5
-  </div>
+  <!-- Button 1 -->
+  <div class="button">Button 1</div>
+
+  <!-- Button 2 -->
+  <div class="button">Button 2</div>
+
+  <!-- Button 3 -->
+  <div class="button">Button 3</div>
+
+  <!-- Button 4 -->
+  <div class="button">Button 4</div>
+
+  <!-- Button 5 -->
+  <div class="button">Button 5</div>
 </div>
