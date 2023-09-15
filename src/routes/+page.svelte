af17f4865fed36b74695f1ba7fdbc19a86db679d
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-15 01:54:25 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,40 @@
-<script>
-    export let data; /*initializes the props*/
-    import ProjectCard from '$lib/components/project-card.svelte'
-  </script>
-  
-  
-  <svelte:head>
-    <title>Bourque Portfolio</title>
-  </svelte:head>
-
-  <h1 class="font-bold text-center mb-20 text-5xl">My Portfolio</h1>
-
-  {#each data.authors as { name, intro, picture: { url } }}
-  <div class="flex mb-40 items-end">
-    <div class="mr-6">
-      <h2 class="text-3xl mb-4 font-bold tracking-wider">{name}</h2>
-      <p class="text-xl mb-4">{intro}</p>
-    </div>
-
-    <img class="mask mask-squircle h-48" src={url} alt={name} />
+<!-- Welcome.svelte -->
+<main>
+  <div class="container">
+    <h1>Welcome to Our App</h1>
+    <p>Thank you for choosing everydaywebthings.com </p>
+    <ul>
+      <li>coming soon!</li>
+      <li>coming soon!</li>
+      <li>coming soon!</li>
+    </ul>
+    <p>Feel free to explore and enjoy!</p>
   </div>
-{/each}
+</main>
 
+<style>
+  /* You can style the page as needed */
+  .container {
+    text-align: center;
+    margin: 2rem;
+  }
 
-<div class="grid gap-10 md:grid-cols-4 md:px-10 lg:grid-cols-6 lg:-mx-52">
-  {#each data.projects as { name, slug, description, image }}
-  <ProjectCard {name} {description} url={image[0].url} {slug} />
+  h1 {
+    font-size: 2rem;
+  }
 
-  {/each}
-</div>
+  p {
+    font-size: 1.2rem;
+    margin-top: 1rem;
+  }
 
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    font-size: 1.2rem;
+    margin-top: 0.5rem;
+  }
+</style>
