6b16b94aadaa05061499d905efc5ccf0dea99f17
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-15 14:15:16 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   <div class="button-row">
     <div class="button-wrapper">
       <h2>Generators</h2>
-      <!-- Container for vertical buttons with block display -->
+      <!-- Container for vertical buttons with box-like styling -->
       <div class="vertical-buttons">
         <a href="/projects" class="btn btn-ghost btn-sm rounded-btn">Portfolio</a>
         <a href="/button1" class="btn btn-ghost btn-sm rounded-btn">Button 1</a>
@@ -11,24 +11,39 @@
     </div>
     <div class="button-wrapper">
       <h2>Weather</h2>
-      <a href="/posts" class="btn btn-ghost btn-sm rounded-btn">Blog</a>
+      <!-- Container for vertical buttons with box-like styling -->
+      <div class="vertical-buttons">
+        <a href="/posts" class="btn btn-ghost btn-sm rounded-btn">Blog</a>
+      </div>
     </div>
     <div class="button-wrapper">
       <h2>Calculators</h2>
-      <a href="/about" class="btn btn-ghost btn-sm rounded-btn">About</a>
+      <!-- Container for vertical buttons with box-like styling -->
+      <div class="vertical-buttons">
+        <a href="/about" class="btn btn-ghost btn-sm rounded-btn">About</a>
+      </div>
     </div>
     <!-- Add 3 more buttons below -->
     <div class="button-wrapper">
       <h2>New Button 1</h2>
-      <a href="/new1" class="btn btn-ghost btn-sm rounded-btn">(blank)</a>
+      <!-- Container for vertical buttons with box-like styling -->
+      <div class="vertical-buttons">
+        <a href="/new1" class="btn btn-ghost btn-sm rounded-btn">(blank)</a>
+      </div>
     </div>
     <div class="button-wrapper">
       <h2>New Button 2</h2>
-      <a href="/new2" class="btn btn-ghost btn-sm rounded-btn">(blank)</a>
+      <!-- Container for vertical buttons with box-like styling -->
+      <div class="vertical-buttons">
+        <a href="/new2" class="btn btn-ghost btn-sm rounded-btn">(blank)</a>
+      </div>
     </div>
     <div class="button-wrapper">
       <h2>New Button 3</h2>
-      <a href="/new3" class="btn btn-ghost btn-sm rounded-btn">(blank)</a>
+      <!-- Container for vertical buttons with box-like styling -->
+      <div class="vertical-buttons">
+        <a href="/new3" class="btn btn-ghost btn-sm rounded-btn">(blank)</a>
+      </div>
     </div>
   </div>
 </main>
@@ -36,7 +51,10 @@
 <style>
   .vertical-buttons a {
     display: block;
-    margin-bottom: 10px; /* Adjust the spacing between the buttons */
+    width: 105px;
+    height: 44px;
+    border-radius: 8px;
+    margin-bottom: 10px;
   }
 
 
