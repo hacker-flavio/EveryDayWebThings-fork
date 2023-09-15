673fc850f6dcaedfd56303999e5e9ee8341a15db
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-15 12:39:17 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,51 +1,35 @@
-<!-- Page.svelte -->
+<script>
+  // Define an array of button labels
+  let buttonLabels = ["Button 1", "Button 2", "Button 3", "Button 4", "Button 5"];
+</script>
 
 <style>
-  .menu {
+  /* Add styles for the button container */
+  .button-container {
     display: flex;
-    flex-direction: row; /* Ensure items are in a horizontal line */
-    justify-content: space-between;
-    align-items: center;
-    padding: 1inch 0;
+    flex-direction: column;
+    gap: 10px; /* Adjust the gap between rows */
   }
 
-  .menuItem {
-    margin: 0 1.5inch; /* 1.5 inches of space between each menuItem */
-  }
-
-  .buttonContainer {
-    display: flex;
-    flex-direction: column; /* Arrange buttons vertically */
-    align-items: center;
-    margin-top: 0.5inch; /* Half-inch space below Generators */
-  }
-
+  /* Style for each button */
   .button {
     width: 230px;
     height: 103px;
-    background-color: #007bff; /* Example background color */
-    color: #fff; /* Example text color */
+    border-radius: 8px;
+    background-color: #007BFF; /* You can change the background color */
+    color: white; /* Text color */
     display: flex;
-    justify-content: center;
     align-items: center;
-    margin-bottom: 0.5inch; /* Half-inch space between buttons */
+    justify-content: center;
+    cursor: pointer;
+    font-size: 16px; /* Adjust the font size */
   }
 </style>
 
-<div class="menu">
-  <div class="menuItem">Generators</div>
-  <div class="menuItem">Weather</div>
-  <div class="menuItem">Calculators</div>
-  <div class="menuItem">Minigames</div>
-  <div class="menuItem">More</div>
+<div class="button-container">
+  {#each buttonLabels as label (label)}
+    <div class="button">{label}</div>
+  {/each}
 </div>
 
-<div class="menuItem">
-  <div class="buttonContainer">
-    <div class="button">Button 1</div>
-    <div class="button">Button 2</div>
-    <div class="button">Button 3</div>
-    <div class="button">Button 4</div>
-  </div>
-</div>
-
+ 
