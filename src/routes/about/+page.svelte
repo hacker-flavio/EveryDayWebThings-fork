0c70a1398426dd6787f781d60418a047ddc5fb62
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-15 01:45:45 -0500

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,16 +1,10 @@
 
-<!-- FeatureUnavailable.svelte -->
-<script>
-  // You can add any logic here if needed
-</script>
 
 <main>
   <div class="container">
     <h1>This Feature is Not Available Yet</h1>
-    <p>Check back later for updates or join our Discord server for more information:</p>
-    <a href="YOUR_DISCORD_INVITE_LINK" target="_blank" rel="noopener noreferrer">
-      <button class="discord-button">Join Our Discord</button>
-    </a>
+    <p>Check back later for updates.</p>
+    <p>Status: 503 Service Unavailable</p>
   </div>
 </main>
 
@@ -29,27 +23,7 @@
     font-size: 1.2rem;
     margin-top: 1rem;
   }
-
-  .discord-button {
-    background-color: #7289da;
-    color: #fff;
-    padding: 1rem 2rem;
-    border: none;
-    border-radius: 4px;
-    font-size: 1.2rem;
-    cursor: pointer;
-  }
-
-  .discord-button:hover {
-    background-color: #677bc4;
-  }
 </style>
-
-
-
-
-
-
 
 
 
