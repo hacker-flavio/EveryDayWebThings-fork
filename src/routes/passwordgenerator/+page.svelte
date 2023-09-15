2314d5a7452aeb176a9ff797eaf01d912777d9d2
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-15 14:58:40 -0500

    Update +page.svelte

--- src/routes/passwordgenerator/+page.svelte
+++ src/routes/passwordgenerator/+page.svelte
@@ -1,34 +1,19 @@
 
+<script>
+  import PasswordGenerator from './PasswordGenerator.svelte';
+</script>
+
 <main>
-  <div class="container">
-    <h1>This Feature is Not Available Yet</h1>
-    <p>Check back later for updates.</p>
-    <p>Status: 503 Service Unavailable</p>
-  </div>
+  <h1>Password Generator Page</h1>
+  <PasswordGenerator />
 </main>
 
 <style>
-  /* You can style the page as needed */
-  .container {
+  main {
     text-align: center;
-    margin: 2rem;
-  }
-
-  h1 {
-    font-size: 2rem;
-  }
-
-  p {
-    font-size: 1.2rem;
-    margin-top: 1rem;
+    font-family: Arial, sans-serif;
   }
 </style>
-
-
-
-
-
-
 
 
 
