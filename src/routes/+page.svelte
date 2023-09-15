a6d1b95c73988cfb23a41bfac4c8c7fa9949b8b1
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-15 13:06:07 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,40 @@
 <script>
-  // You can define any logic or data here if needed
+  // You can define any logic or data you need here
 </script>
 
 <style>
-  /* Style the container for the button row */
   .button-row {
     display: flex;
     justify-content: space-between;
-    align-items: center;
+    margin: 1inch 0;
   }
 
-  /* Style the individual buttons */
   .button {
     width: 230px;
     height: 103px;
     border-radius: 8px;
-    background-color: #0074d9; /* You can change the background color */
-    color: white; /* Text color */
-    font-size: 18px; /* Text size */
+    background-color: #007bff; /* You can change the background color */
+    color: white;
     display: flex;
-    justify-content: center;
     align-items: center;
+    justify-content: center;
     cursor: pointer;
-    margin: 5px; /* Adjust the margin between buttons as needed */
   }
 </style>
 
 <div class="button-row">
-  <!-- Button 1 -->
-  <div class="button">Button 1</div>
-
-  <!-- Button 2 -->
-  <div class="button">Button 2</div>
-
-  <!-- Button 3 -->
-  <div class="button">Button 3</div>
-
-  <!-- Button 4 -->
-  <div class="button">Button 4</div>
-
-  <!-- Button 5 -->
-  <div class="button">Button 5</div>
+  {#each [1, 2, 3, 4, 5] as buttonIndex}
+    <div class="button" on:click={() => handleClick(buttonIndex)}>
+      Button {buttonIndex}
+    </div>
+  {/each}
 </div>
+
+<script>
+  // You can add any logic or event handling functions here
+
+  function handleClick(buttonIndex) {
+    // Handle button click for the specified button (buttonIndex)
+    console.log(`Button ${buttonIndex} clicked`);
+  }
+</script>
