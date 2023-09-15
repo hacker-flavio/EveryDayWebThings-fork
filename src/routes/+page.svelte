5de29536e26abbb96848417cd06bc7a00032797d
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-15 13:41:46 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,11 @@
 <main>
   <h1>Button Page</h1>
   <div class="button-row">
-    <a href="/about" class="button-link">About</a>
-    <a href="/about1" class="button-link">About1</a>
-    <a href="/about2" class="button-link">About2</a>
-    <a href="/about3" class="button-link">About3</a>
-    <a href="/about4" class="button-link">About4</a>
+    <a href="/projects" class="btn btn-ghost btn-sm rounded-btn">Portfolio</a>
+    <a href="/posts" class="btn btn-ghost btn-sm rounded-btn">Blog</a>
+    <a href="/about" class="btn btn-ghost btn-sm rounded-btn">About</a>
+    <a href="/blank1" class="btn btn-ghost btn-sm rounded-btn">(blank)</a>
+    <a href="/blank2" class="btn btn-ghost btn-sm rounded-btn">(blank)</a>
   </div>
 </main>
 
@@ -19,19 +19,4 @@
     justify-content: space-between;
     margin: 1inch;
   }
-
-  .button-link {
-    background-color: #007bff; /* You can set your desired background color */
-    color: #fff;
-    border: none;
-    cursor: pointer;
-    text-decoration: none;
-    padding: 10px 20px;
-    border-radius: 8px;
-  }
-  
-  .button-link:hover {
-    background-color: #0056b3; /* Change the color on hover if desired */
-  }
 </style>
-
