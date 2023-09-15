bc7f78cdec783c00190fe975ac9b5debbd9d6e09
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-15 13:58:45 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <main>
- 
   <div class="button-row">
     <div class="button-wrapper">
       <h2>Generators</h2>
@@ -18,20 +17,54 @@
       <a href="/blank1" class="btn btn-ghost btn-sm rounded-btn">(blank)</a>
     </div>
     <div class="button-wrapper">
-      <h2>others</h2>
+      <h2>Others</h2>
       <a href="/blank2" class="btn btn-ghost btn-sm rounded-btn">(blank)</a>
     </div>
   </div>
+
+  <!-- Additional rows of buttons -->
+  <div class="button-row">
+    <div class="button-wrapper">
+      <h2>Row 2 - Button 1</h2>
+      <a href="/row2-button1" class="btn btn-ghost btn-sm rounded-btn">Row 2 Button 1</a>
+    </div>
+    <div class="button-wrapper">
+      <h2>Row 2 - Button 2</h2>
+      <a href="/row2-button2" class="btn btn-ghost btn-sm rounded-btn">Row 2 Button 2</a>
+    </div>
+    <div class="button-wrapper">
+      <h2>Row 2 - Button 3</h2>
+      <a href="/row2-button3" class="btn btn-ghost btn-sm rounded-btn">Row 2 Button 3</a>
+    </div>
+  </div>
+
+  <div class="button-row">
+    <div class="button-wrapper">
+      <h2>Row 3 - Button 1</h2>
+      <a href="/row3-button1" class="btn btn-ghost btn-sm rounded-btn">Row 3 Button 1</a>
+    </div>
+    <div class="button-wrapper">
+      <h2>Row 3 - Button 2</h2>
+      <a href="/row3-button2" class="btn btn-ghost btn-sm rounded-btn">Row 3 Button 2</a>
+    </div>
+    <div class="button-wrapper">
+      <h2>Row 3 - Button 3</h2>
+      <a href="/row3-button3" class="btn btn-ghost btn-sm rounded-btn">Row 3 Button 3</a>
+    </div>
+  </div>
+
+  <div class="button-row">
+    <div class="button-wrapper">
+      <h2>Row 4 - Button 1</h2>
+      <a href="/row4-button1" class="btn btn-ghost btn-sm rounded-btn">Row 4 Button 1</a>
+    </div>
+    <div class="button-wrapper">
+      <h2>Row 4 - Button 2</h2>
+      <a href="/row4-button2" class="btn btn-ghost btn-sm rounded-btn">Row 4 Button 2</a>
+    </div>
+    <div class="button-wrapper">
+      <h2>Row 4 - Button 3</h2>
+      <a href="/row4-button3" class="btn btn-ghost btn-sm rounded-btn">Row 4 Button 3</a>
+    </div>
+  </div>
 </main>
-
-<style>
-  .button-row {
-    display: flex;
-    justify-content: space-between;
-    margin: 1inch;
-  }
-
-  .button-wrapper {
-    text-align: center;
-  }
-</style>
