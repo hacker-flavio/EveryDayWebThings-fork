db56e6b6b811a2982a9e30715b95c6bf6223bfd4
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-15 13:33:20 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,41 @@
 <script>
-  function handleClick(buttonIndex) {
-    console.log(`Button ${buttonIndex} clicked`);
+  import { goto } from "@sveltejs/kit/router";
+
+  // Define the routes for the buttons
+  const routes = ["/about", "/about1", "/about2", "/about3", "/about4"];
+
+  // Function to navigate to a route
+  function navigateTo(route) {
+    goto(route);
   }
 </script>
+
+<main>
+  <h1>Button Page</h1>
+  <div class="button-row">
+    {#each routes as route (route)}
+      <button
+        on:click={() => navigateTo(route)}
+        class="button"
+        style="width: 230px; height: 103px; border-radius: 8px; margin-right: 1rem;"
+      >
+        {route}
+      </button>
+    {/each}
+  </div>
+</main>
 
 <style>
   .button-row {
     display: flex;
     justify-content: space-between;
-    margin: 1inch 0;
+    margin: 1inch;
   }
 
   .button {
-    width: 230px;
-    height: 103px;
-    border-radius: 8px;
-    background-color: #007bff; /* You can change the background color */
-    color: white;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    background-color: #007bff; /* You can set your desired background color */
+    color: #fff;
+    border: none;
     cursor: pointer;
   }
 </style>
-
-<div class="button-row">
-  {#each [1, 2, 3, 4, 5] as buttonIndex}
-    <div class="button" on:click={() => handleClick(buttonIndex)}>
-      Button {buttonIndex}
-    </div>
-  {/each}
-</div>
