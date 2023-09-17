02836c84824d3d5c28a31a7c37be41163819dd47
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-17 15:44:16 -0500

    Add files via upload

---
+++ src/components/RandomWordGenerator.svelte
@@ -0,0 +1,29 @@
+<script>
+  let randomWord = '';
+
+  const generateRandomWord = () => {
+    // Replace this with your own method to generate random words
+    const words = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
+    const randomIndex = Math.floor(Math.random() * words.length);
+    randomWord = words[randomIndex];
+  };
+</script>
+
+<div>
+  <h2>Random Word Generator</h2>
+  <button on:click="{generateRandomWord}">Generate Random Word</button>
+  {#if randomWord}
+    <p>The random word is: {randomWord}</p>
+  {/if}
+</div>
+
+
+
+
+
+
+
+
+
+
+
