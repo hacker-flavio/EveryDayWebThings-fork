bd08dc1f33bb5098d91aae559f14ced6033cbc10
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-17 15:54:47 -0500

    Update +page.svelte

--- src/routes/RandomWordGenerator/+page.svelte
+++ src/routes/RandomWordGenerator/+page.svelte
@@ -8,43 +8,6 @@
 
 
 
-<style>
-  div {
-    text-align: center;
-    margin: 20px;
-    padding: 20px;
-    border: 2px solid #333;
-    border-radius: 5px;
-    background-color: #f5f5f5;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-  }
-
-  h2 {
-    font-size: 1.5rem;
-    margin-bottom: 10px;
-  }
-
-  button {
-    padding: 10px 20px;
-    font-size: 1rem;
-    background-color: #007bff;
-    color: #fff;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: background-color 0.3s ease;
-  }
-
-  button:hover {
-    background-color: #0056b3;
-  }
-
-  p {
-    font-size: 1.25rem;
-    margin-top: 10px;
-  }
-</style>
-
 
 
 
