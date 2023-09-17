40722d2e53300cee7b6c6363bd0d05930988fd9d
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-17 15:44:08 -0500

    Update +page.svelte

--- src/routes/RandomWordGenerator/+page.svelte
+++ src/routes/RandomWordGenerator/+page.svelte
@@ -1,29 +1,11 @@
 
 
-<main>
-  <div class="container">
-    <h1>This Feature is Not Available Yet</h1>
-    <p>Check back later for updates.</p>
-    <p>Status: 503 Service Unavailable</p>
-  </div>
-</main>
+<script>
+  import RandomWordGenerator from './RandomWordGenerator.svelte';
+</script>
 
-<style>
-  /* You can style the page as needed */
-  .container {
-    text-align: center;
-    margin: 2rem;
-  }
+<RandomWordGenerator />
 
-  h1 {
-    font-size: 2rem;
-  }
-
-  p {
-    font-size: 1.2rem;
-    margin-top: 1rem;
-  }
-</style>
 
 
 
