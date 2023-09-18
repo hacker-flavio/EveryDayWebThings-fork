416b4f2350c3458bad8603ffcb83fed604f6be06
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-18 01:21:45 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -49,22 +49,48 @@
 </main>
 
 <style>
+.vertical-buttons a {
+  display: block;
+  width: 150px; /* Increase the width for better readability */
+  height: 44px;
+  border-radius: 8px;
+  margin-bottom: 10px;
+  text-decoration: none; /* Remove underlines from links */
+  text-align: center; /* Center text horizontally */
+  line-height: 44px; /* Vertically center text */
+  background-color: #3498db; /* Background color for buttons */
+  color: #fff; /* Text color for buttons */
+  font-weight: bold;
+  transition: background-color 0.3s ease; /* Smooth hover effect */
+}
+
+.vertical-buttons a:hover {
+  background-color: #2980b9; /* Background color on hover */
+}
+
+.button-row {
+  display: flex;
+  flex-wrap: wrap; /* Wrap button wrappers to the next line if needed */
+  justify-content: space-between;
+  margin: 1inch;
+}
+
+.button-wrapper {
+  text-align: center;
+  margin-bottom: 20px; /* Increase the spacing between button groups */
+}
+
+/* Optional: Add some space between button headings */
+.button-wrapper h2 {
+  margin-top: 0;
+  margin-bottom: 10px;
+}
+
+/* Optional: Adjust the size of the buttons for small screens */
+@media (max-width: 768px) {
   .vertical-buttons a {
-    display: block;
-    width: 105px;
-    height: 44px;
-    border-radius: 8px;
-    margin-bottom: 10px;
+    width: 100%;
   }
+}
 
-
-  .button-row {
-    display: flex;
-    justify-content: space-between;
-    margin: 1inch;
-  }
-
-  .button-wrapper {
-    text-align: center;
-  }
 </style>
