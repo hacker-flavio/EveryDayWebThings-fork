bb524d1bb0541f176a4f963772e6963907c1f5f0
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-18 01:04:41 -0500

    Update +page.svelte

--- src/routes/SimpleWeatherInfo/+page.svelte
+++ src/routes/SimpleWeatherInfo/+page.svelte
@@ -1,28 +1,16 @@
 
 
+<!-- App.svelte -->
+<script>
+  import Weather from '../../SimpleWeatherInfo.svelte';
+</script>
+
 <main>
-  <div class="container">
-    <h1>This Feature is Not Available Yet</h1>
-    <p>Check back later for updates.</p>
-    <p>Status: 503 Service Unavailable</p>
-  </div>
+  <Weather />
 </main>
 
 <style>
-  /* You can style the page as needed */
-  .container {
-    text-align: center;
-    margin: 2rem;
-  }
-
-  h1 {
-    font-size: 2rem;
-  }
-
-  p {
-    font-size: 1.2rem;
-    margin-top: 1rem;
-  }
+  /* Add your CSS styles here */
 </style>
 
 
