1ce8bc799ca8e8642c283b0c5fccfbae20cc032a
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-18 00:03:28 -0500

    Update +page.svelte

--- src/routes/RandomQuoteGenerator/+page.svelte
+++ src/routes/RandomQuoteGenerator/+page.svelte
@@ -1,29 +1,10 @@
 
+<script>
+  import RandomQuoteGenerator from '../../components/RandomQuoteGenerator.svelte';
+</script>
 
-<main>
-  <div class="container">
-    <h1>This Feature is Not Available Yet</h1>
-    <p>Check back later for updates.</p>
-    <p>Status: 503 Service Unavailable</p>
-  </div>
-</main>
+<RandomQuoteGenerator />
 
-<style>
-  /* You can style the page as needed */
-  .container {
-    text-align: center;
-    margin: 2rem;
-  }
-
-  h1 {
-    font-size: 2rem;
-  }
-
-  p {
-    font-size: 1.2rem;
-    margin-top: 1rem;
-  }
-</style>
 
 
 
