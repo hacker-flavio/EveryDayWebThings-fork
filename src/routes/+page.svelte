9988229380441b7efe2281e06db7717c708e4e21
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-18 01:27:10 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -51,44 +51,40 @@
 
 
 <style>
-  /* Common button styles */
   .vertical-buttons a {
-    display: block;
-    width: 150px; /* Adjust the width as needed */
-    height: 50px; /* Adjust the height as needed */
+    display: inline-block;
+    padding: 10px 20px;
+    font-size: 16px;
+    text-align: center;
+    text-decoration: none;
+    border: 2px solid #3498db;
     border-radius: 8px;
     margin-bottom: 10px;
-    text-align: center;
-    line-height: 50px; /* Center the text vertically */
-    font-size: 16px; /* Adjust the font size as needed */
-    text-decoration: none;
-    color: #333; /* Adjust the text color as needed */
-    background-color: #f0f0f0; /* Adjust the background color as needed */
-    transition: background-color 0.3s ease; /* Smooth hover transition */
-  }
+    color: #3498db;
+    transition: background-color 0.3s;
+}
 
-  /* Button hover effect */
-  .vertical-buttons a:hover {
-    background-color: #ddd; /* Adjust the hover background color as needed */
-  }
+.vertical-buttons a:hover {
+    background-color: #3498db;
+    color: #ffffff;
+}
 
-  /* Main container for buttons */
-  .button-row {
+.button-wrapper h2 {
+    font-size: 24px;
+    margin-bottom: 10px;
+    color: #333333;
+}
+
+.button-row {
     display: flex;
-    flex-wrap: wrap; /* Wrap buttons to the next row if necessary */
     justify-content: space-between;
     margin: 1inch;
-  }
+}
 
-  /* Individual button container */
-  .button-wrapper {
+.button-wrapper {
     text-align: center;
-    width: 33%; /* Adjust the width for 3 columns */
-  }
+    max-width: 200px; /* Added max-width to prevent content from stretching too much */
+}
 
-  /* Optional: Add some spacing around the buttons */
-  .button-wrapper h2 {
-    margin-bottom: 10px;
-  }
 </style>
 
