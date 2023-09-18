1f8d4fed60c0f335a28168be308f1e209acfb447
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-18 00:40:55 -0500

    Update +page.svelte

--- src/routes/BasicCalculator/+page.svelte
+++ src/routes/BasicCalculator/+page.svelte
@@ -1,34 +1,13 @@
 
 
-<main>
-  <div class="container">
-    <h1>This Feature is Not Available Yet</h1>
-    <p>Check back later for updates.</p>
-    <p>Status: 503 Service Unavailable</p>
-  </div>
-</main>
+<!-- App.svelte -->
+<script>
+  import Calculator from '../../components/Calculator.svelte";
+</script>
 
-<style>
-  /* You can style the page as needed */
-  .container {
-    text-align: center;
-    margin: 2rem;
-  }
-
-  h1 {
-    font-size: 2rem;
-  }
-
-  p {
-    font-size: 1.2rem;
-    margin-top: 1rem;
-  }
-</style>
-
-
-
-
-
+<div class="app">
+  <Calculator />
+</div>
 
 
 
