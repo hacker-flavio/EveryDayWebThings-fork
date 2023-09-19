5cd8485cb77c1b5ef16e0a68e12e5afe987882ba
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-18 19:14:52 -0500

    Create BudgetCalculator.svelte

---
+++ src/components/BudgetCalculator.svelte
@@ -0,0 +1,48 @@
+<script>
+  let income = 0;
+  let expenses = 0;
+  let total = 0;
+
+  function calculateTotal() {
+    total = income - expenses;
+  }
+</script>
+
+<main>
+  <h1>Budget Calculator</h1>
+  
+  <label>
+    Income:
+    <input type="number" bind:value="{income}" on:input="{calculateTotal}" />
+  </label>
+
+  <label>
+    Expenses:
+    <input type="number" bind:value="{expenses}" on:input="{calculateTotal}" />
+  </label>
+
+  <p>Total: {total}</p>
+</main>
+
+<style>
+  main {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    max-width: 400px;
+    margin: auto;
+  }
+
+  label {
+    display: block;
+    margin: 10px 0;
+  }
+
+  input {
+    width: 100%;
+    padding: 5px;
+  }
+
+  p {
+    font-weight: bold;
+  }
+</style>
