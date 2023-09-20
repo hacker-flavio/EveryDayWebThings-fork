b19c8082e4e94bad6342cdfc70a91108f23167f1
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-20 15:21:28 -0500

    Update +page.svelte

--- src/routes/DigitalWhiteboard/+page.svelte
+++ src/routes/DigitalWhiteboard/+page.svelte
@@ -13,4 +13,67 @@
     text-align: center;
     margin: 50px auto;
   }
+  /* Styles for buttons */
+  .button-row {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 10px;
+    margin-top: 20px;
+  }
+
+  .button-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    background-color: #f0f0f0;
+    border: 1px solid #ccc;
+    padding: 15px;
+    border-radius: 5px;
+    width: 200px;
+  }
+
+  .button-wrapper h2 {
+    font-size: 18px;
+    margin-bottom: 10px;
+  }
+
+  .vertical-buttons a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 40px;
+    padding: 0 10px;
+    font-size: 16px;
+    margin: 5px 0;
+    border: 2px solid #3498db;
+    border-radius: 8px;
+    text-decoration: none;
+    color: #3498db;
+    transition: background-color 0.3s, color 0.3s;
+  }
+
+  .vertical-buttons a:hover {
+    background-color: #3498db;
+    color: #fff;
+  }
+
+  /* Other styles */
+  .whiteboard-container {
+    position: relative;
+  }
+
+  .controls {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 20px;
+  }
+
+  .color-pickers {
+    display: flex;
+    gap: 10px;
+  }
 </style>
