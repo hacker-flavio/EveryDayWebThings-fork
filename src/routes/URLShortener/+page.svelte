35a37ab557424eb6b98563c62ea474659086ffae
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-20 22:16:09 -0500

    Update +page.svelte

--- src/routes/URLShortener/+page.svelte
+++ src/routes/URLShortener/+page.svelte
@@ -1,31 +1,12 @@
+<!-- URLShortenerPage.svelte -->
 
+<script>
+  import URLShortener from '../../components/URLShortener.svelte';
+</script>
 
 <main>
-  <div class="container">
-    <h1>This Feature is Not Available Yet</h1>
-    <p>Check back later for updates.</p>
-    <p>Status: 503 Service Unavailable</p>
-  </div>
+  <URLShortener />
 </main>
-
-<style>
-  /* You can style the page as needed */
-  .container {
-    text-align: center;
-    margin: 2rem;
-  }
-
-  h1 {
-    font-size: 2rem;
-  }
-
-  p {
-    font-size: 1.2rem;
-    margin-top: 1rem;
-  }
-</style>
-
-
 
 
 
