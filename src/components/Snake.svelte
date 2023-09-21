fcd42ef30f55a70ba41f8e8c70f8cdc975bf7b9b
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-21 01:28:41 -0500

    Update Snake.svelte

--- src/components/Snake.svelte
+++ src/components/Snake.svelte
@@ -1,1 +1,26 @@
+<script>
+  let snake = [{x: 10, y: 10}];
+  let direction = {x: 1, y: 0};
+  
+  function update() {
+    const head = snake[0];
+    const newHead = {x: head.x + direction.x, y: head.y + direction.y};
+    snake = [newHead, ...snake.slice(0, -1)];
+  }
+  
+  setInterval(update, 100);
+</script>
+
+<style>
+  div {
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    background-color: green;
+  }
+</style>
+
+{#each snake as {x, y} (x)}
+  <div style="left:{x*20}px;top:{y*20}px;"></div>
+{/each}
 
