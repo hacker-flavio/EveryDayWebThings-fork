49319276737e94f12165e0315361d2bdbac71b59
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-20 19:31:48 -0500

    Update RandomNumberGenerator.svelte

--- src/components/RandomNumberGenerator.svelte
+++ src/components/RandomNumberGenerator.svelte
@@ -1,1 +1,22 @@
+<!-- RandomNumberGenerator.svelte -->
+
+<script>
+  let randomNumber = null;
+
+  const generateRandomNumber = () => {
+    randomNumber = Math.floor(Math.random() * 100); // Change the range as needed
+  };
+</script>
+
+<style>
+  /* Add your CSS styles here */
+</style>
+
+<div>
+  <h1>Random Number Generator</h1>
+  <button on:click={generateRandomNumber}>Generate Random Number</button>
+  {#if randomNumber !== null}
+    <p>Random Number: {randomNumber}</p>
+  {/if}
+</div>
 
