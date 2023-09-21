639fd22d84f98edebc9db7ab90ce95afac29a7f4
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-21 01:49:04 -0500

    Update Snake.svelte

--- src/components/Snake.svelte
+++ src/components/Snake.svelte
@@ -60,14 +60,45 @@
 
   // Initialize the game
   generateFood();
-  setInterval(update, 200); // Update the game every 200 milliseconds
+  let gameInterval = setInterval(update, 200); // Update the game every 200 milliseconds
+
+  // Cleanup function to stop the game interval when the component is destroyed
+  onDestroy(() => {
+    clearInterval(gameInterval);
+  });
 </script>
 
-<div class="grid" style="display: grid; grid-template-columns: repeat({numCols}, ${gridSize}px); grid-template-rows: repeat({numRows}, ${gridSize}px); width: {gridSize * numCols}px; height: {gridSize * numRows}px; border: 1px solid #000;">
+<div class="grid">
   {#each Array(numRows) as _, row}
     {#each Array(numCols) as _, col}
-      <div style="width: {gridSize - 2}px; height: {gridSize - 2}px; background-color: #ccc;" class="{snake.some(segment => segment.x === col && segment.y === row) ? 'snake' : ''} {food.x === col && food.y === row ? 'food' : ''}"></div>
+      <div
+        class="cell {snake.some(segment => segment.x === col && segment.y === row) ? 'snake' : ''} {food.x === col && food.y === row ? 'food' : ''}"
+      ></div>
     {/each}
   {/each}
 </div>
 
+<style>
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(${numCols}, ${gridSize}px);
+    grid-template-rows: repeat(${numRows}, ${gridSize}px);
+    width: ${gridSize * numCols}px;
+    height: ${gridSize * numRows}px;
+    border: 1px solid #000;
+  }
+
+  .cell {
+    width: ${gridSize - 2}px;
+    height: ${gridSize - 2}px;
+    background-color: #ccc;
+  }
+
+  .snake {
+    background-color: #00f;
+  }
+
+  .food {
+    background-color: #f00;
+  }
+</style>
