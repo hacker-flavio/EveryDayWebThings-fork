0c2132e82bd8b4fa0665e1330e34b06173644ab0
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-21 01:29:52 -0500

    Update +page.svelte

--- src/routes/Snake/+page.svelte
+++ src/routes/Snake/+page.svelte
@@ -1,29 +1,10 @@
 
+<script>
+  import Snake from '../../components/Snake.svelte';
+</script>
 
-<main>
-  <div class="container">
-    <h1>This Feature is Not Available Yet</h1>
-    <p>Check back later for updates.</p>
-    <p>Status: 503 Service Unavailable</p>
-  </div>
-</main>
+<Snake />
 
-<style>
-  /* You can style the page as needed */
-  .container {
-    text-align: center;
-    margin: 2rem;
-  }
-
-  h1 {
-    font-size: 2rem;
-  }
-
-  p {
-    font-size: 1.2rem;
-    margin-top: 1rem;
-  }
-</style>
 
 
 
