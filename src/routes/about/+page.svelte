92232925280f8b5e4f757995e8514b94d33c84e0
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-20 22:22:56 -0500

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,27 +1,45 @@
-
-
 <main>
   <div class="container">
-    <h1>This Feature is Not Available Yet</h1>
-    <p>Check back later for updates.</p>
-    <p>Status: 503 Service Unavailable</p>
+    <h1>Oops! This Feature is Not Available Yet</h1>
+    <p>Please check back later for updates.</p>
+    <p>Status: <span class="status">503 Service Unavailable</span></p>
   </div>
 </main>
 
 <style>
   /* You can style the page as needed */
+  body {
+    font-family: Arial, sans-serif;
+    background-color: #f0f0f0;
+    margin: 0;
+    padding: 0;
+  }
+
   .container {
     text-align: center;
-    margin: 2rem;
+    margin: 2rem auto;
+    background-color: #fff;
+    border-radius: 10px;
+    padding: 2rem;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    max-width: 400px;
   }
 
   h1 {
     font-size: 2rem;
+    color: #333;
+    margin-bottom: 1rem;
   }
 
   p {
     font-size: 1.2rem;
-    margin-top: 1rem;
+    margin: 0.5rem 0;
+    color: #666;
+  }
+
+  .status {
+    font-weight: bold;
+    color: #e74c3c; /* Red color for status */
   }
 </style>
 
