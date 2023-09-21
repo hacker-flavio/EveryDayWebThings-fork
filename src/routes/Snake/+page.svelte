fe72bc76d1d7597f18e6a11717cc6a83fd2cf777
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-21 01:37:22 -0500

    Update +page.svelte

--- src/routes/Snake/+page.svelte
+++ src/routes/Snake/+page.svelte
@@ -3,18 +3,14 @@
   import Snake from '../../components/Snake.svelte';
 </script>
 
-<Snake />
+<div>
+  <h1>Snake Game</h1>
+  <Snake />
+</div>
 
-
-
-
-
-
-
-
-
-
-
+<style>
+  /* Add your main page styles here */
+</style>
 
 
 
