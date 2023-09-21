9d55c90e00732ce3180717f7ff505c07652e650f
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-20 19:33:15 -0500

    Update +page.svelte

--- src/routes/RandomNumberGenerator/+page.svelte
+++ src/routes/RandomNumberGenerator/+page.svelte
@@ -1,29 +1,15 @@
+<!-- RandomNumberGeneratorPage.svelte -->
 
+<script>
+  import RandomNumberGenerator from '../../components/RandomNumberGenerator.svelte';
+</script>
 
 <main>
-  <div class="container">
-    <h1>This Feature is Not Available Yet</h1>
-    <p>Check back later for updates.</p>
-    <p>Status: 503 Service Unavailable</p>
-  </div>
+  <RandomNumberGenerator />
 </main>
 
-<style>
-  /* You can style the page as needed */
-  .container {
-    text-align: center;
-    margin: 2rem;
-  }
 
-  h1 {
-    font-size: 2rem;
-  }
 
-  p {
-    font-size: 1.2rem;
-    margin-top: 1rem;
-  }
-</style>
 
 
 
