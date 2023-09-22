67166935b0d33abbd1d5094c716a950fba613068
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-21 19:21:46 -0500

    Update RandomQuoteGenerator.svelte

--- src/components/RandomQuoteGenerator.svelte
+++ src/components/RandomQuoteGenerator.svelte
@@ -20,3 +20,40 @@
     <p>"{randomQuote}"</p>
   {/if}
 </div>
+
+
+
+<style>
+        /* Style the container div */
+        div {
+            text-align: center;
+            margin: 20px;
+        }
+
+        /* Style the heading */
+        h2 {
+            font-size: 24px;
+            color: #333;
+        }
+
+        /* Style the button */
+        button {
+            background-color: #3498db;
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            font-size: 18px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #2980b9;
+        }
+
+        /* Style the quote paragraph */
+        p {
+            font-size: 18px;
+            margin-top: 10px;
+            color: #555;
+        }
+    </style>
