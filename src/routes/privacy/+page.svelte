28183e2b1e602e53df35e0b2e4446fd2010e5f33
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-22 01:35:32 -0500

    Update +page.svelte

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -8,16 +8,69 @@
 <main>
   <section class="privacy-policy">
     <h1>Privacy Policy</h1>
+    <p>Last Updated: 9/22/23</p>
 
-    <p>Your privacy is important to us. It is our policy to respect your privacy regarding any information we may collect from you across our website.</p>
+    <p>EveryDayWebThings ("us," "we," or "our") is committed to protecting your privacy. This Privacy Policy explains how your personal information is collected, used, and disclosed by us.</p>
 
-    <!-- ... Rest of your content ... -->
+    <h2>Information We Collect</h2>
+
+    <p>We may collect information that you provide directly to us. For example, we collect information when you:</p>
+
+    <ul>
+      <li>Allow access to your location for weather information</li>
+      <li>Use our services with an account</li>
+      <li>Contact customer support</li>
+      <li>Participate in surveys or promotions</li>
+    </ul>
+
+    <p>The information we may collect includes your name, email address, username, password, and any other information you choose to provide.</p>
+
+    <h2>How We Use Your Information</h2>
+
+    <p>We may use your information for various purposes, including:</p>
+
+    <ul>
+      <li>To provide, maintain, and improve our services</li>
+      <li>To send you updates, newsletters, or marketing communications</li>
+      <li>To respond to your comments, questions, or requests</li>
+      <li>To monitor and analyze usage patterns and trends</li>
+    </ul>
+
+    <h2>Sharing of Your Information</h2>
+
+    <p>We may share your information with third parties only in the following circumstances:</p>
+
+    <ul>
+      <li>With your consent</li>
+      <li>To comply with legal obligations</li>
+      <li>To protect our rights, privacy, safety, or property</li>
+      <li>In connection with the sale, merger, or acquisition of all or part of our company</li>
+    </ul>
+
+    <h2>Your Choices</h2>
+
+    <p>You may have various rights regarding your personal information, including:</p>
+
+    <ul>
+      <li>Access the personal information we hold about you</li>
+      <li>Correct inaccuracies in your personal information</li>
+      <li>Withdraw your consent</li>
+      <li>Delete your personal information</li>
+    </ul>
+
+    <p>To exercise any of these rights, please contact us at <a href="mailto:Contact@everydaywebthings.com">Contact@everydaywebthings.com</a></p>
+
+    <h2>Security</h2>
+
+    <p>We take reasonable measures to help protect your personal information from unauthorized access, use, or disclosure.</p>
+
+    <h2>Changes to this Privacy Policy</h2>
+
+    <p>We may update this Privacy Policy from time to time. The updated policy will be posted on this page with the "Last Updated" date revised accordingly.</p>
 
     <h2>Contact Us</h2>
 
-    <p>If you have any questions about this Privacy Policy, please contact us:</p>
-
-    <p>Email: your.email@example.com</p>
+    <p>If you have any questions or concerns about this Privacy Policy, please contact us at <a href="mailto:Contact@everydaywebthings.com">Contact@everydaywebthings.com</a></p>
   </section>
 </main>
 
@@ -45,11 +98,21 @@
     font-size: 16px;
     line-height: 1.5;
   }
+
+  ul {
+    list-style-type: disc;
+    margin-left: 20px;
+  }
+
+  a {
+    color: #007bff;
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
 </style>
-
-
-
-
 
 
 
