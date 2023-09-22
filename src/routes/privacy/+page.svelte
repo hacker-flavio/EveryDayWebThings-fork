b347c2f6b261677bdd5db46351d9a0808a240e63
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-22 01:27:10 -0500

    Update +page.svelte

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -1,34 +1,51 @@
 
+<!-- PrivacyPolicy.svelte -->
+
+<script>
+  // Add any necessary data or logic here
+</script>
 
 <main>
-  <div class="container">
-    <h1>This Feature is Not Available Yet</h1>
-    <p>Check back later for updates.</p>
-    <p>Status: 503 Service Unavailable</p>
-  </div>
+  <section class="privacy-policy">
+    <h1>Privacy Policy</h1>
+
+    <p>Your privacy is important to us. It is our policy to respect your privacy regarding any information we may collect from you across our website.</p>
+
+    <!-- ... Rest of your content ... -->
+
+    <h2>Contact Us</h2>
+
+    <p>If you have any questions about this Privacy Policy, please contact us:</p>
+
+    <p>Email: your.email@example.com</p>
+  </section>
 </main>
 
 <style>
-  /* You can style the page as needed */
-  .container {
-    text-align: center;
-    margin: 2rem;
+  /* Add your CSS styles here to customize the page's appearance */
+
+  .privacy-policy {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
   }
 
   h1 {
-    font-size: 2rem;
+    font-size: 28px;
+    margin-bottom: 20px;
+  }
+
+  h2 {
+    font-size: 24px;
+    margin-top: 20px;
+    margin-bottom: 10px;
   }
 
   p {
-    font-size: 1.2rem;
-    margin-top: 1rem;
+    font-size: 16px;
+    line-height: 1.5;
   }
 </style>
-
-
-
-
-
 
 
 
