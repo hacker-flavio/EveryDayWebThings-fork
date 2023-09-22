ce1a327f868166cbbcf8c5dbf6195bba667c53ae
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-21 19:25:26 -0500

    Update RandomQuoteGenerator.svelte

--- src/components/RandomQuoteGenerator.svelte
+++ src/components/RandomQuoteGenerator.svelte
@@ -23,17 +23,22 @@
 
 
 
-<style>
+ <style>
         /* Style the container div */
         div {
             text-align: center;
             margin: 20px;
+            background-color: #f7f7f7;
+            border-radius: 10px;
+            padding: 20px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
         }
 
         /* Style the heading */
         h2 {
-            font-size: 24px;
+            font-size: 28px;
             color: #333;
+            margin-bottom: 10px;
         }
 
         /* Style the button */
@@ -41,9 +46,11 @@
             background-color: #3498db;
             color: #fff;
             border: none;
-            padding: 10px 20px;
-            font-size: 18px;
+            padding: 12px 24px;
+            font-size: 20px;
             cursor: pointer;
+            border-radius: 5px;
+            transition: background-color 0.3s;
         }
 
         button:hover {
@@ -52,8 +59,9 @@
 
         /* Style the quote paragraph */
         p {
-            font-size: 18px;
-            margin-top: 10px;
+            font-size: 20px;
+            margin-top: 20px;
             color: #555;
+            font-style: italic;
         }
     </style>
