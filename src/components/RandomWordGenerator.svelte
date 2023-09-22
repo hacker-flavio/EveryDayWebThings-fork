de1682216bc245087edcfab7298956357246edeb
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-21 20:37:36 -0500

    Update RandomWordGenerator.svelte

--- src/components/RandomWordGenerator.svelte
+++ src/components/RandomWordGenerator.svelte
@@ -1,131 +1,78 @@
+<!-- src/RandomWordGenerator.svelte -->
 <script>
-let randomWords = [];
+  import { onMount } from 'svelte';
 
-const generateRandomWords = () => {
-  const wordCount = document.getElementById('wordCount').value;
-  randomWords = [];
-  
-  for (let i = 0; i < wordCount; i++) {
-    const categories = [
-      ['apple', 'banana', 'cherry', 'date', 'elderberry'],
-      ['dog', 'cat', 'rabbit', 'hamster', 'parrot'],
-      ['ocean', 'forest', 'desert', 'mountain', 'river'],
-      ['car', 'bicycle', 'bus', 'train', 'airplane'],
-      ['book', 'movie', 'music', 'painting', 'sculpture'],
-      ['computer', 'smartphone', 'tablet', 'laptop', 'gaming console'],
-      ['pizza', 'sushi', 'burger', 'pasta', 'salad'],
-      ['sunglasses', 'hat', 'scarf', 'gloves', 'umbrella'],
-      ['guitar', 'piano', 'drums', 'violin', 'flute'],
-      ['sunflower', 'rose', 'tulip', 'daisy', 'lily'],
-      ['coffee', 'tea', 'juice', 'water', 'soda'],
-      ['beach', 'mountain', 'city', 'countryside', 'island'],
-      ['camera', 'binoculars', 'telescope', 'microscope', 'magnifier'],
-      ['football', 'basketball', 'soccer', 'tennis', 'volleyball'],
-      ['sweater', 'jacket', 'shirt', 'dress', 'jeans'],
-      ['fireplace', 'candles', 'lantern', 'flashlight', 'torch'],
-      ['tiger', 'elephant', 'lion', 'giraffe', 'rhinoceros'],
-      ['planet', 'star', 'moon', 'comet', 'asteroid'],
-      ['keyboard', 'mouse', 'monitor', 'printer', 'speakers'],
-      ['broccoli', 'carrot', 'spinach', 'potato', 'pepper'],
-      ['boat', 'kayak', 'canoe', 'yacht', 'submarine'],
-      ['globe', 'map', 'compass', 'GPS', 'atlas'],
-      ['glasses', 'contacts', 'sunglasses', 'goggles', 'monocle'],
-      ['eagle', 'hawk', 'falcon', 'vulture', 'owl'],
-      ['clock', 'watch', 'hourglass', 'sundial', 'calendar'],
-      ['swimming', 'hiking', 'cycling', 'running', 'yoga'],
-      ['couch', 'chair', 'table', 'bookshelf', 'lamp'],
-      ['diamond', 'emerald', 'sapphire', 'ruby', 'amethyst'],
-      ['paintbrush', 'canvas', 'easel', 'palette', 'watercolors'],
-      ['soccer', 'basketball', 'tennis', 'golf', 'baseball'],
-      ['suitcase', 'backpack', 'duffel bag', 'briefcase', 'tote'],
-      ['kite', 'frisbee', 'yo-yo', 'hula hoop', 'juggling balls'],
-      ['ballet', 'hip-hop', 'salsa', 'tango', 'breakdancing'],
-      ['sunscreen', 'umbrella', 'hat', 'sunglasses', 'beach towel'],
-      ['seashell', 'coral', 'seaweed', 'starfish', 'crab'],
-      ['microphone', 'guitar', 'drums', 'keyboard', 'saxophone'],
-      ['pencil', 'pen', 'marker', 'eraser', 'highlighter'],
-      ['telescope', 'microscope', 'binoculars', 'magnifying glass', 'camera'],
-      ['teapot', 'coffee maker', 'kettle', 'French press', 'espresso machine'],
-      ['camera', 'binoculars', 'telescope', 'microscope', 'magnifier'],
-      ['strawberry', 'blueberry', 'raspberry', 'blackberry', 'cranberry'],
-      ['television', 'radio', 'smartphone', 'tablet', 'laptop'],
-      ['moonlight', 'starlight', 'candlelight', 'lamplight', 'firelight'],
-      ['skateboard', 'rollerblades', 'scooter', 'hoverboard', 'bike'],
-      ['penguin', 'seal', 'walrus', 'narwhal', 'orca'],
-      ['mountain', 'canyon', 'plateau', 'valley', 'hill'],
-      ['flowerpot', 'vase', 'planter', 'urn', 'terrarium'],
-      ['banana', 'grape', 'watermelon', 'kiwi', 'pineapple'],
-      ['helicopter', 'hot air balloon', 'glider', 'paraglider', 'biplane'],
-      // Add more categories with unique words as needed.
-    ];
+  let selectedCategory = '';
+  let randomWord = '';
 
-     const randomCategoryIndex = Math.floor(Math.random() * categories.length);
-    const randomCategory = categories[randomCategoryIndex];
+  let categories = [
+    'Animals',
+    'Fruits',
+    'Colors',
+    'Countries',
+    'Sports',
+    'Vegetables',
+    'Vehicles',
+    'Professions',
+    'Hobbies',
+    'Weather',
+    'Emotions',
+    'Body Parts',
+    'Shapes',
+    'Planets',
+    'Insects',
+    'Furniture',
+    'Clothing',
+    'Music Genres',
+    'Desserts',
+  ];
 
-    const randomIndex = Math.floor(Math.random() * randomCategory.length);
-    const word = randomCategory[randomIndex];
+  let words = {
+    'Animals': ['Dog', 'Cat', 'Elephant', 'Lion', 'Monkey'],
+    'Fruits': ['Apple', 'Banana', 'Orange', 'Grapes', 'Strawberry'],
+    'Colors': ['Red', 'Blue', 'Green', 'Yellow', 'Purple'],
+    'Countries': ['USA', 'Canada', 'France', 'Japan', 'Australia'],
+    'Sports': ['Football', 'Basketball', 'Soccer', 'Tennis', 'Swimming'],
+    'Vegetables': ['Carrot', 'Broccoli', 'Tomato', 'Cucumber', 'Spinach'],
+    'Vehicles': ['Car', 'Bicycle', 'Bus', 'Motorcycle', 'Train'],
+    'Professions': ['Doctor', 'Teacher', 'Engineer', 'Artist', 'Chef'],
+    'Hobbies': ['Reading', 'Painting', 'Cooking', 'Gardening', 'Photography'],
+    'Weather': ['Sunny', 'Rainy', 'Cloudy', 'Snowy', 'Windy'],
+    'Emotions': ['Happy', 'Sad', 'Angry', 'Surprised', 'Excited'],
+    'Body Parts': ['Hand', 'Foot', 'Head', 'Heart', 'Eye'],
+    'Shapes': ['Circle', 'Square', 'Triangle', 'Rectangle', 'Oval'],
+    'Planets': ['Earth', 'Mars', 'Venus', 'Jupiter', 'Saturn'],
+    'Insects': ['Ant', 'Butterfly', 'Spider', 'Bee', 'Ladybug'],
+    'Furniture': ['Chair', 'Table', 'Sofa', 'Bed', 'Cabinet'],
+    'Clothing': ['Shirt', 'Pants', 'Dress', 'Shoes', 'Hat'],
+    'Music Genres': ['Rock', 'Pop', 'Hip-Hop', 'Jazz', 'Country'],
+    'Desserts': ['Cake', 'Ice Cream', 'Cookies', 'Pie', 'Brownie'],
+  };
 
-    randomWords.push(word);
+  function generateRandomWord() {
+    let categoryWords = words[selectedCategory];
+    randomWord = categoryWords[Math.floor(Math.random() * categoryWords.length)];
   }
-};
+
+  onMount(() => {
+    generateRandomWord();
+  });
 </script>
 
+<main>
+  <h1>Random Word Generator</h1>
 
+  <label for="category">Select a Category:</label>
+  <select id="category" bind:value="{selectedCategory}">
+    {#each categories as category (category)}
+      <option value="{category}">{category}</option>
+    {/each}
+  </select>
 
+  <button on:click="{generateRandomWord}">Generate Random Word</button>
 
-
-
-<style>
-  /* Add your CSS styles here */
-  div {
-    text-align: center;
-    margin: 20px;
-    background-color: #f7f7f7;
-    border-radius: 10px;
-    padding: 20px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-  }
-
-  h2 {
-    font-size: 28px;
-    color: #333;
-    margin-bottom: 10px;
-  }
-
-  button {
-    background-color: #3498db;
-    color: #fff;
-    border: none;
-    padding: 12px 24px;
-    font-size: 20px;
-    cursor: pointer;
-    border-radius: 5px;
-    transition: background-color 0.3s;
-  }
-
-  button:hover {
-    background-color: #2980b9;
-  }
-
-  p {
-    font-size: 20px;
-    margin-top: 20px;
-    color: #555;
-  }
-</style>
-
-<div>
-  <h2>Random Word Generator</h2>
-  <label for="wordCount">Number of Words (1-10):</label>
-  <input type="number" id="wordCount" min="1" max="10" value="1">
-  <button on:click="{generateRandomWords}">Generate Random Words</button>
-  {#if randomWords && randomWords.length > 0}
-    <p>The random words are: {randomWords.join(', ')}</p>
-  {/if}
-</div>
-
-
-
+  <p>Random Word: {randomWord}</p>
+</main>
 
 
 
