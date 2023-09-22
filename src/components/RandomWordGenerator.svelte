e0c178dd8e5989289706933f93e6f1a407934b37
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-21 20:47:09 -0500

    Update RandomWordGenerator.svelte

--- src/components/RandomWordGenerator.svelte
+++ src/components/RandomWordGenerator.svelte
@@ -3,7 +3,8 @@
   import { onMount } from 'svelte';
 
   let selectedCategory = 'All'; // Set the default category to 'All'
-  let randomWord = '';
+  let randomWords = [];
+  let numberOfWords = 1; // Default number of words
 
   let categories = [
     'All',
@@ -20,21 +21,31 @@
     // Add more words for each category as needed
   };
 
-  function generateRandomWord() {
+  function generateRandomWords() {
     if (selectedCategory === 'All') {
       const allWords = categories
         .filter(category => category !== 'All')
         .map(category => words[category])
         .flat();
-      randomWord = allWords[Math.floor(Math.random() * allWords.length)];
+
+      randomWords = [];
+      for (let i = 0; i < numberOfWords; i++) {
+        const randomIndex = Math.floor(Math.random() * allWords.length);
+        randomWords.push(allWords[randomIndex]);
+      }
     } else {
       let categoryWords = words[selectedCategory];
-      randomWord = categoryWords[Math.floor(Math.random() * categoryWords.length)];
+
+      randomWords = [];
+      for (let i = 0; i < numberOfWords; i++) {
+        const randomIndex = Math.floor(Math.random() * categoryWords.length);
+        randomWords.push(categoryWords[randomIndex]);
+      }
     }
   }
 
   onMount(() => {
-    generateRandomWord();
+    generateRandomWords();
   });
 </script>
 
@@ -48,9 +59,21 @@
     {/each}
   </select>
 
-  <button on:click="{generateRandomWord}">Generate Random Word</button>
+  <label for="numberOfWords">Number of Words:</label>
+  <input type="number" id="numberOfWords" bind:value="{numberOfWords}" min="1">
 
-  <p>Random Word: {randomWord}</p>
+  <button on:click="{generateRandomWords}">Generate Random Words</button>
+
+  {#if randomWords.length > 0}
+    <p>Random Words:</p>
+    <ul>
+      {#each randomWords as word (word)}
+        <li>{word}</li>
+      {/each}
+    </ul>
+  {:else}
+    <p>No words generated yet.</p>
+  {/if}
 </main>
 
 <style>
