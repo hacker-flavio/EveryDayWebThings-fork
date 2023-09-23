46d03bd14c69389f4fd7f406962e34b053c8171a
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-23 15:56:28 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,48 +8,71 @@
         <a href="/RandomQuoteGenerator" class="btn btn-ghost btn-sm rounded-btn">Random Quote Generator</a>
         <a href="/RandomParagraphGenerator" class="btn btn-ghost btn-sm rounded-btn">Random Paragraph Generator</a>
         <a href="/RandomNumberGenerator" class="btn btn-ghost btn-sm rounded-btn">Random Number Generator</a>
+        <a href="/DummyImageGgenerator" class="btn btn-ghost btn-sm rounded-btn">Dummy Image Ggenerator</a>
+        <a href="/PlaceholderImageGenerator" class="btn btn-ghost btn-sm rounded-btn">Placeholder Image Generator</a>
+        <a href="/RandomColorGenerator" class="btn btn-ghost btn-sm rounded-btn">Random Color Generator</a>
+        <a href="/QrCodeGenerator" class="btn btn-ghost btn-sm rounded-btn">Qr Code Generator</a>
+        <a href="/ColorPaletteGenerator" class="btn btn-ghost btn-sm rounded-btn">Color Palette Generator</a>
       </div>
     </div>
 
     <div class="button-wrapper">
-      <h2>Weather</h2>
+      <h2>Productivity and Organization</h2>
       <div class="vertical-buttons">
+        <a href="/DigitalWhiteboard" class="btn btn-ghost btn-sm rounded-btn">DigitalWhiteboard</a>
         <a href="/SimpleWeatherInfo" class="btn btn-ghost btn-sm rounded-btn">Simple Weather Info</a>
-        <a href="/SimpleWeather" class="btn btn-ghost btn-sm rounded-btn">Weather Info</a>
+        <a href="/BasicTranslator" class="btn btn-ghost btn-sm rounded-btn">Basic Translator</a>
+        <a href="/URLShortener" class="btn btn-ghost btn-sm rounded-btn">URL Shortener</a>
+        <a href="/Timer" class="btn btn-ghost btn-sm rounded-btn">Timer</a>
+        <a href="/StopWatch" class="btn btn-ghost btn-sm rounded-btn">StopWatch</a>
+        <a href="/FlashCardMaker" class="btn btn-ghost btn-sm rounded-btn">Flash Card Maker</a>
+        <a href="/DigitalCountdownCalendar" class="btn btn-ghost btn-sm rounded-btn">Digital Countdown Calendar</a>
+        <a href="/Countdown" class="btn btn-ghost btn-sm rounded-btn">Countdown</a>
+        <a href="/QuickPollCreator" class="btn btn-ghost btn-sm rounded-btn">Quick Poll Creator</a>
       </div>
     </div>
 
     <div class="button-wrapper">
       <h2>Calculators</h2>
       <div class="vertical-buttons">
-        <a href="/BasicCalculator" class="btn btn-ghost btn-sm rounded-btn">Basic Calculator</a>
+        <a href="/BasicCalculator" class="btn btn-ghost btn-sm rounded-btn">Standard Calculator</a>
         <a href="/BudgetCalculator" class="btn btn-ghost btn-sm rounded-btn">Budget Calculator</a>
         <a href="/ScientificCalculator" class="btn btn-ghost btn-sm rounded-btn">Scientific Calculator</a>
+        <a href="/SquareRootCalculator" class="btn btn-ghost btn-sm rounded-btn">Square Root Calculator</a>
+        <a href="/PrecentageCalculator" class="btn btn-ghost btn-sm rounded-btn">Precentage Calculator</a>
+
+
       </div>
     </div>
 
     <div class="button-wrapper">
-      <h2>Other tools</h2>
+      <h2>Image Tools</h2>
       <div class="vertical-buttons">
-        <a href="/URLShortener" class="btn btn-ghost btn-sm rounded-btn">URL Shortener</a>
-        <a href="/DigitalWhiteboard" class="btn btn-ghost btn-sm rounded-btn">DigitalWhiteboard</a>
-        <a href="/Timer" class="btn btn-ghost btn-sm rounded-btn">Timer</a>
-        <a href="/Stopwatch" class="btn btn-ghost btn-sm rounded-btn">Stopwatch</a>
+
+        <a href="/ImageResizer" class="btn btn-ghost btn-sm rounded-btn">Image Resizer</a>
+        <a href="/ImageConverter" class="btn btn-ghost btn-sm rounded-btn">Image Converter</a>
+        <a href="/ImageCompressor" class="btn btn-ghost btn-sm rounded-btn">Image Compressor</a>
+        <a href="/PlaceholderImageMaker" class="btn btn-ghost btn-sm rounded-btn">Placeholder Image Maker</a>
       </div>
     </div>
 
     <div class="button-wrapper">
-      <h2>New Button 2</h2>
+      <h2>Writing tools</h2>
       <div class="vertical-buttons">
-        <a href="/new2" class="btn btn-ghost btn-sm rounded-btn">Button</a>
+        <a href="/WordFormater" class="btn btn-ghost btn-sm rounded-btn">Word Formater</a>
+        <a href="/WordCounter" class="btn btn-ghost btn-sm rounded-btn">Word Counter</a>
+        <a href="/TitleCapitlization" class="btn btn-ghost btn-sm rounded-btn">Title Capitlization</a>
       </div>
     </div>
 
     <div class="button-wrapper">
-      <h2>Mini Games</h2>
+      <h2>Entertainment and Daily Content</h2>
       <div class="vertical-buttons">
         <a href="/TikTacToe" class="btn btn-ghost btn-sm rounded-btn">Tik Tac Toe</a>
         <a href="/Snake" class="btn btn-ghost btn-sm rounded-btn">Snake</a>
+        <a href="/JokeoftheDay" class="btn btn-ghost btn-sm rounded-btn">Joke of the Day</a>
+        <a href="/DailyFunFact" class="btn btn-ghost btn-sm rounded-btn">Daily Fun Fact</a>
+        <a href="/DailyBrainTeaser" class="btn btn-ghost btn-sm rounded-btn">DailyBrainTeaser</a>
       </div>
     </div>
   </div>
