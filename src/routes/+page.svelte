5bffe9ad5f010488bb0d16b1ab5e36e3ec8ba483
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-23 16:53:50 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -83,85 +83,78 @@
 
 
 <style>
-    /* Center the entire main content */
-body {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
-    margin: 0;
-    background-color: #f0f0f0; /* Add a background color */
-}
+        /* Center the entire main content */
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
 
-main {
-    text-align: center;
-}
+        main {
+            text-align: center;
+        }
 
-/* Buttons styling */
-.vertical-buttons a {
-    display: inline-block;
-    width: calc(33.33% - 20px); /* Distribute buttons in rows of three per row */
-    height: 80px; /* Increase button height */
-    padding: 14px 24px; /* Improved padding for better spacing */
-    font-size: 18px;
-    margin: 10px; /* Increased margin for better spacing */
-    border: 2px solid #3498db;
-    border-radius: 8px;
-    text-decoration: none;
-    color: #3498db;
-    transition: background-color 0.3s, color 0.3s;
-    background-color: #fff; /* Initial background color */
-}
+        /* Updated CSS for desktop */
+        .vertical-buttons a {
+            display: block;
+            width: 100%;
+            height: 60px;
+            padding: 10px 0;
+            font-size: 18px;
+            margin: 5px 0;
+            border: 2px solid #3498db;
+            border-radius: 8px;
+            text-decoration: none;
+            color: #3498db;
+            transition: background-color 0.3s, color 0.3s;
+        }
 
-.vertical-buttons a:hover {
-    background-color: #3498db;
-    color: #fff;
-}
+        .vertical-buttons a:hover {
+            background-color: #3498db;
+            color: #fff;
+        }
 
-/* Button wrapper */
-.button-wrapper h2 {
-    font-size: 20px; /* Increase the font size */
-    margin-bottom: 12px; /* Enhanced margin */
-    color: #333;
-    font-weight: bold; /* Add bold font weight */
-}
+        .button-wrapper h2 {
+            font-size: 18px;
+            margin-bottom: 10px;
+            color: #333;
+        }
 
-/* Flex container for buttons */
-.button-row {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    margin: 0 -10px; /* Improved spacing */
-}
+        .button-row {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            margin: 0 -10px;
+        }
 
-/* Individual button container */
-.button-wrapper {
-    text-align: center;
-    flex-grow: 1;
-    margin: 10px;
-}
+        .button-wrapper {
+            text-align: center;
+            flex-grow: 1;
+            margin: 10px;
+            flex-basis: calc(33.33% - 20px); /* Adjust to control button width */
+        }
 
-/* Mobile optimization using media query */
-@media (max-width: 768px) {
-    .vertical-buttons a {
-        width: 100%; /* Full width for mobile */
-        height: 60px; /* Adjust height for smaller screens */
-        font-size: 16px;
-        margin: 8px; /* Adjusted margin */
-    }
+        /* Mobile optimization using media query */
+        @media (max-width: 768px) {
+            .vertical-buttons a {
+                height: 50px;
+                font-size: 16px;
+                margin: 5px 0;
+            }
 
-    .button-wrapper h2 {
-        font-size: 18px; /* Adjusted font size */
-        margin-bottom: 10px; /* Adjusted margin */
-    }
+            .button-wrapper h2 {
+                font-size: 16px;
+                margin-bottom: 5px;
+            }
 
-    .button-row {
-        flex-direction: column;
-    }
+            .button-row {
+                flex-direction: column;
+            }
 
-    .button-wrapper {
-        flex-basis: calc(100% - 20px);
-    }
-}
-
+            .button-wrapper {
+                flex-basis: calc(100% - 20px);
+            }
+        }
     </style>
