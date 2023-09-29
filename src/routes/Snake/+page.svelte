761d87541c0036ee3dd917f6e79c0505a3b8637d
Samthibau <Samthibault28@gmail.com>
2023-09-29 16:07:14 -0500

    update

--- src/routes/Snake/+page.svelte
+++ src/routes/Snake/+page.svelte
@@ -1,16 +1,1 @@
 
-<script>
-  import Snake from '../../components/Snake.svelte';
-</script>
-
-<div>
-  <h1>Snake Game</h1>
-  <Snake />
-</div>
-
-<style>
-  /* Add your main page styles here */
-</style>
-
-
-
