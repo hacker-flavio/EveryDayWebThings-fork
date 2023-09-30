c39b252abbe826241b643e887d235e9c18419dfd
Samthibau <Samthibault28@gmail.com>
2023-09-30 01:36:23 -0500

    hn

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,36 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
-import adapterStatic from '@sveltejs/adapter-static'; // Import the adapter-static module
+import adapterStatic from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-	},
+  kit: {
+    adapter: adapter(),
+    
+    // Add the fallback option here
+    // You can specify a URL or a function
+    // Example 1: Specify a URL to a fallback page
+    fallback: 'fallback.html',
 
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	]
+    // Example 2: Specify a function for custom fallback logic
+    // fallback: async (request) => {
+    //   // Implement your custom fallback logic here
+    //   return new Response('Custom Fallback Page', { status: 404 });
+    // },
+  },
+
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
 };
 
 // Add the adapter-static configuration
 if (process.env.NODE_ENV === 'production') {
-	config.kit.adapter = adapterStatic({
-		// Specify the paths to serve
-		// Adjust this path to point to your translations directory
-		assets: 'translations',
-	});
+  config.kit.adapter = adapterStatic({
+    assets: 'translations',
+  });
 }
 
 export default config;
