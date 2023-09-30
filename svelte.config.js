3dc1d81bb86893ec8fc682607e9c5cb7fe4306e0
Samthibau <Samthibault28@gmail.com>
2023-09-30 01:32:51 -0500

    Updates

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,11 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
+import adapterStatic from '@sveltejs/adapter-static'; // Import the adapter-static module
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
 	},
 
 	preprocess: [
@@ -13,5 +14,14 @@ const config = {
 		})
 	]
 };
+
+// Add the adapter-static configuration
+if (process.env.NODE_ENV === 'production') {
+	config.kit.adapter = adapterStatic({
+		// Specify the paths to serve
+		// Adjust this path to point to your translations directory
+		assets: 'translations',
+	});
+}
 
 export default config;
